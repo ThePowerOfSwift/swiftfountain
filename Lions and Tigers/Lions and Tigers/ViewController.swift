c56f54003a2414ac0bd4640edc7507939c19b6e2
Stanley Chiang <stan@Stanleys-Mac-Pro.local>
2014-09-18 20:25:08 -0400

    implemented non repeating random pics with transitions

--- Lions and Tigers/Lions and Tigers/ViewController.swift
+++ Lions and Tigers/Lions and Tigers/ViewController.swift
@@ -14,28 +14,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var ageLabel: UILabel!
     @IBOutlet weak var breedLabel: UILabel!
-    
+
+    var tigers:[Tiger] = []
+    var rando:Int = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-
-        var tigers:[Tiger] = []
+        // Do any additional setup after loading the view, typically from a nib.ds
         
-        var myTiger = Tiger(age: 1, name:"ben", breed: "bengal", image: UIImage(named: "BengalTiger.jpg"))
+//        var myTiger = Tiger(age: 1, name:"ben", breed: "bengal", image: UIImage(named: "BengalTiger.jpg"))
         
         var tiger2 = Tiger(age: 2, name:"sibal", breed: "siberian", image: UIImage(named: "SiberianTiger.jpg"))
-        
         var tiger3 = Tiger(age: 3, name:"mani", breed: "malyan", image: UIImage(named: "MalayanTiger.jpg"))
         
-        tigers.append(myTiger)
+        tigers.append(Tiger(age: 1, name:"ben", breed: "bengal", image: UIImage(named: "BengalTiger.jpg")))
         tigers += [tiger2, tiger3]
-//        tigers.append(tiger2)
-//        tigers.append(tiger3)
-        
+
 //        for tiger in tigers{
 //            println(tiger.breed)
 //        }
-        var rando:Int;
         rando = Int(arc4random_uniform(UInt32(tigers.count)))
         
         myimageView.image = tigers[rando].image
@@ -51,7 +48,16 @@ class ViewController: UIViewController {
     }
 
     @IBAction func nextPressed(sender: UIBarButtonItem) {
-
+        
+        do {rando = Int(arc4random_uniform(UInt32(tigers.count)))}
+        while nameLabel.text == tigers[rando].name
+        //println(rando)
+        UIView.transitionWithView(self.view, duration: 0.2, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
+            self.myimageView.image = self.tigers[self.rando].image
+            self.nameLabel.text = self.tigers[self.rando].name
+            self.breedLabel.text = self.tigers[self.rando].breed
+            self.ageLabel.text = "\(self.tigers[self.rando].age)"
+            }, completion: { (finished: Bool) -> () in
+        })
     }
-
 }
