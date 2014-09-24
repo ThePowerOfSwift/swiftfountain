6e04a4a6fcf7b5bb3de3dbdfe3daeb0707723345
stanchiang <stanchiang23@gmail.com>
2014-09-23 21:45:59 -0400

    adding second container elements

--- Lemonade Stand/Lemonade Stand/ViewController.swift
+++ Lemonade Stand/Lemonade Stand/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
 	var startSelling: UIView!
 	
 	//constants
-	let kViewMargin:CGFloat = 10.0
+	let kSixth:CGFloat = 1/6
 	let kFourth:CGFloat = 1/4
 	let kThird:CGFloat = 1/3
 	let kHalf:CGFloat = 1/2
@@ -27,6 +27,22 @@ class ViewController: UIViewController {
 	var bank: UILabel!
 	var lemonInv: UILabel!
 	var iceInv: UILabel!
+	
+	//second container: labels
+	var step1Label: UILabel!
+	var lemPriceLabel: UILabel!
+	var icePriceLabel: UILabel!
+	var buyLemLabel: UILabel!
+	var buyIceLabel: UILabel!
+	
+	//second container: buttons
+	var moarLemsButton: UIButton!
+	var lessLemsButton: UIButton!
+	var moarIceButton: UIButton!
+	var lessIceButton: UIButton!
+	
+	//misc
+	var blackBorder:CGColor = UIColor.blackColor().CGColor
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -45,9 +61,9 @@ class ViewController: UIViewController {
 	}
 	
 	func setupContainerViews() {
-		var x = self.view.bounds.origin.x + kViewMargin
+		var x = self.view.bounds.origin.x
 		var y = self.view.bounds.origin.y
-		var width = self.view.bounds.width - kViewMargin * 2
+		var width = self.view.bounds.width
 		var height = self.view.bounds.height * kFourth
 		
 		inventory = UIView(frame: CGRectMake(x, y, width, height))
@@ -55,55 +71,114 @@ class ViewController: UIViewController {
 		mix = UIView(frame: CGRectMake(x, inventory.frame.height + supplies.frame.height, width, height))
 		startSelling = UIView(frame: CGRectMake(x, inventory.frame.height + supplies.frame.height + mix.frame.height, width, height))
 
-		inventory.backgroundColor = UIColor.grayColor()
-		supplies.backgroundColor = UIColor.greenColor()
-		mix.backgroundColor = UIColor.grayColor()
-		startSelling.backgroundColor = UIColor.redColor()
+//		inventory.backgroundColor = UIColor.grayColor()
+		supplies.backgroundColor = UIColor.grayColor()
+//		mix.backgroundColor = UIColor.grayColor()
+//		startSelling.backgroundColor = UIColor.redColor()
 		
 		self.view.addSubview(inventory)
 		self.view.addSubview(supplies)
-		self.view.addSubview(mix)
-		self.view.addSubview(startSelling)
+//		self.view.addSubview(mix)
+//		self.view.addSubview(startSelling)
 	}
 	
 	func firstContainer(cont: UIView) {
-		var x:CGFloat = 20
-		var y:CGFloat = 30
+		var x:CGFloat = cont.frame.origin.x + 20
+		var y:CGFloat = cont.frame.origin.y + 30
 		var width = self.view.bounds.width * kHalf
 		var height = self.view.bounds.height
 		
-		youHaveLabel = UILabel(frame: CGRectMake(x, y, width, height))
+		youHaveLabel = UILabel(frame: CGRectMake(x, y, width, height * kThird))
 		youHaveLabel.text = "You have:"
 		youHaveLabel.textColor = UIColor.redColor()
 		youHaveLabel.font = UIFont(name: "MarkerFeld-Wide", size: 20)
 		youHaveLabel.sizeToFit()
 
-		bank = UILabel()
+		bank = UILabel(frame: CGRectMake(width, y, width, height))
 		bank.text = "$10"
 		bank.textColor = UIColor.greenColor()
 		bank.font = UIFont(name: "MarkerFeld-Wide", size: 20)
 		bank.sizeToFit()
 		
-		lemonInv = UILabel()
+		lemonInv = UILabel(frame: CGRectMake(width, y + cont.bounds.height * kThird, width, height))
 		lemonInv.text = "1 Lemons"
 		lemonInv.textColor = UIColor.blackColor()
 		lemonInv.font = UIFont(name: "MarkerFeld-Wide", size: 20)
 		lemonInv.sizeToFit()
 		
-		iceInv = UILabel()
+		iceInv = UILabel(frame: CGRectMake(width, y + cont.bounds.height * 2 * kThird, width, height))
 		iceInv.text = "1 Ice Cubes"
 		iceInv.textColor = UIColor.blackColor()
 		iceInv.font = UIFont(name: "MarkerFeld-Wide", size: 20)
 		iceInv.sizeToFit()
 		
 		cont.addSubview(youHaveLabel)
-//		cont.addSubview(bank)
-//		cont.addSubview(lemonInv)
-//		cont.addSubview(iceInv)
+		cont.addSubview(bank)
+		cont.addSubview(lemonInv)
+		cont.addSubview(iceInv)
 	}
 	
 	func secondContainer(cont: UIView) {
+		var x:CGFloat = cont.bounds.origin.x
+		var y:CGFloat = cont.bounds.origin.y
+		var width:CGFloat = cont.bounds.width
+		var height:CGFloat = cont.bounds.height
+
+		step1Label = UILabel(frame: CGRectMake(x, y, width, height))
+		step1Label.text = "Step 1: Purchase Supplies"
+		step1Label.textColor = UIColor.purpleColor()
+		step1Label.font = UIFont(name: "MarkerFeld-Wide", size: 20)
+		step1Label.sizeToFit()
 		
+		lemPriceLabel = UILabel(frame: CGRectMake(x, y + height * kThird, width * kHalf, height * kThird))
+		lemPriceLabel.text = "Lemons for $2:"
+		lemPriceLabel.textColor = UIColor.blackColor()
+		lemPriceLabel.textAlignment = NSTextAlignment.Right
+		lemPriceLabel.font = UIFont(name: "MarkerFeld-Wide", size: 20)
+		
+		icePriceLabel = UILabel(frame: CGRectMake(x, y + height * 2 * kThird, width * kHalf, height * kThird))
+		icePriceLabel.text = "Ice Cubes for $1:"
+		icePriceLabel.textColor = UIColor.blackColor()
+		icePriceLabel.textAlignment = NSTextAlignment.Right
+		icePriceLabel.font = UIFont(name: "MarkerFeld-Wide", size: 20)
+		
+		buyLemLabel = UILabel(frame: CGRectMake(x + width * 4 * kSixth, y + height * kThird, width * kSixth, height * kThird))
+		buyLemLabel.text = "0"
+		buyLemLabel.textColor = UIColor.blackColor()
+		buyLemLabel.textAlignment = NSTextAlignment.Center
+		buyLemLabel.font = UIFont(name: "MarkerFeld-Wide", size: 20)
+
+		buyIceLabel = UILabel(frame: CGRectMake(x + width * 4 * kSixth, y + height * 2 * kThird, width * kSixth, height * kThird))
+		buyIceLabel.text = "0"
+		buyIceLabel.textColor = UIColor.blackColor()
+		buyIceLabel.textAlignment = NSTextAlignment.Center
+		buyIceLabel.font = UIFont(name: "MarkerFeld-Wide", size: 20)
+		
+		
+		moarIceButton = UIButton()
+		lessLemsButton = UIButton()
+		lessIceButton = UIButton()
+		
+		moarLemsButton = UIButton(frame: CGRectMake(x + width * 3 * kSixth, y + height * kThird, width * kSixth, height * kThird))
+		moarLemsButton.setTitle("+", forState: UIControlState.Normal)
+		moarLemsButton.setTitleColor(UIColor.purpleColor(), forState: UIControlState.Normal)
+		moarLemsButton.titleLabel?.font = UIFont(name: "MarkerFeld-Wide", size: 20)
+		moarLemsButton.center = CGPointMake(x + width * 3 * kSixth, y + height * kThird)
+		moarLemsButton.addTarget(self, action: "addLemsPressed:", forControlEvents: UIControlEvents.TouchUpInside)
+
+		moarLemsButton.layer.borderColor = blackBorder
+		moarLemsButton.layer.borderWidth = 2
+		
+		cont.addSubview(step1Label)
+		cont.addSubview(lemPriceLabel)
+		cont.addSubview(icePriceLabel)
+		cont.addSubview(buyLemLabel)
+		cont.addSubview(buyIceLabel)
+		
+		cont.addSubview(moarLemsButton)
+//		cont.addSubview(moarIceButton)
+//		cont.addSubview(lessLemsButton)
+//		cont.addSubview(lessIceButton)
 	}
 	
 	func thirdContainer(cont: UIView) {
@@ -113,4 +188,27 @@ class ViewController: UIViewController {
 	func fourthContainer(cont: UIView) {
 		
 	}
+
+	func addLemsPressed(button: UIButton) {
+		buyLemLabel.text = "1"
+	}
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
