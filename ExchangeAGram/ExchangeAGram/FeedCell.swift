a3b4cf2c330874bc51a93aa6041fb75d776f9149
Stanley Chiang <stan@Stanleys-MBP.attlocal.net>
2014-10-25 20:18:05 -0400

    init and setting up camera controller

---
+++ ExchangeAGram/ExchangeAGram/FeedCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Stanley Chiang on 10/25/14.
+//  Copyright (c) 2014 Stanley Chiang. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+}
